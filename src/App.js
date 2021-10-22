943f4d0fc3d53e010db2ee2dff64907030a9a6a0
viennp84 <viennp@hotmail.com>
2021-10-22 16:14:20 -0700

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './style.css';
+import TodoList from './TodoList';
 function App() {
-  return null;
+  return <TodoList />;
 }
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function TodoList() {
+  return <div>Hello world</div>;
+}
