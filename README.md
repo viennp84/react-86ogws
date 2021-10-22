710847add6ce63699f70faa5974efdb71a5197f9
viennp84 <viennp@hotmail.com>
2021-10-22 16:04:21 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-86ogws
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-86ogws)
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './style.css';
+function App() {
+  return <TodoList />;
+}
+
+export default App;
